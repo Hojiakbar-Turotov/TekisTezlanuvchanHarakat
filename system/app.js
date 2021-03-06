5af2b50643428b0af021cfc9dc42ad28e6cfe51a
Hojiakbar-Turotov <73689981+Hojiakbar-Turotov@users.noreply.github.com>
2021-03-06 21:25:06 +0500

    juda yaxshi

--- system/app.js
+++ system/app.js
@@ -1,13 +1,46 @@
 const g = 9.81; 
-let alfa = 6;
-let sinalfa = Math.sin(alfa * Math.PI / 180);
-let cosalfa = Math.cos(alfa * Math.PI / 180);
-let myu = 0.009;
+ let isBoolean = true;
 
-let a = g * (sinalfa - myu * cosalfa);
-console.log(a);
+const alfa = document.getElementById('burchak');
+const alfatekshiruv = document.getElementById('burchakMosligi');
 
-let S = 0.8;
+alfa.onkeyup = function() {
+    if(alfa.value > 90){
+        isBoolean = false;
+        alfatekshiruv.innerHTML = `burchak 90&#176; dan katta bo'lishi mumkin emas! `;
+    } else if(alfa.value < 0){
+        isBoolean = false;
+        alfatekshiruv.innerHTML = `burchak manfiy bo'lishi mumkin emas! `;
+    } else{
+        isBoolean = true;
+        alfatekshiruv.innerHTML = ``;
+    }
+}
 
-let t = Math.sqrt(2*S/a);
-console.log(t);
+const myu = document.getElementById("myu");
+const myutekshiruv = document.getElementById('myuMosligi');
+
+myu.onkeyup = function() {
+    if(myu.value > 1){
+        isBoolean = false;
+        myutekshiruv.innerHTML = `burchak 1 dan katta bo'lishi mumkin emas! `;
+    } else if(myu.value < 0){
+        isBoolean = false;
+        myutekshiruv.innerHTML = `burchak manfiy bo'lishi mumkin emas! `;
+    } else{
+        isBoolean = true;
+        myutekshiruv.innerHTML = ``;
+    }
+}
+
+
+const yul = document.getElementById("yul");
+
+
+function hisobla(){
+    if (isBoolean){
+    document.getElementById('vaqt').innerHTML = `t=` + (Math.sqrt(2 * yul.value / (g * (Math.sin(alfa.value * Math.PI / 180) - myu.value * Math.cos(alfa.value * Math.PI / 180))))).toFixed(7) + `s` ;
+    } else {
+        document.getElementById('vaqt').innerHTML = `Xatolik yuz berdi!`;
+    }
+}
